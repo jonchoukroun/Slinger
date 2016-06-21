3154774558b8d9952e1560dd7a45eabc789cc182
jonchoukroun <jonchoukroun@gmail.com>
2016-06-21 16:58:36 -0700

    Change object Drug to class Drug

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-function Drug(name, min, max, scarcity) {
-    this.name = name;
-    this.min = min;
-    this.max = max;
+// JavaScript for DrugWars (script.js)
+
+// Create Drug class (see bottom comment about Scarcity)
+class Drug {
+    constructor(name, min, max) {
+        this.name = name;
+        this.min = min;
+        this.max = max;
+    }
+
+    // Call these 2 functions at each Subway stop
+    calculatePrice() {
+        var price = Math.random() * (this.max - this.min + 1) + this.min;
+        return Math.round(price);
+    };
+
+    calculateQuantity() {
+        var quantity = Math.random() * (50 - 1);
+        return Math.round(quantity);
+    };
 }
 
-Drug.prototype.calculatePrice = function() {
-    var price = Math.random() * (this.max - this.min + 1) + this.min;
-    return Math.round(price);
-};
-
-Drug.prototype.calculateQuantity = function() {
-    var quantity = Math.random() * (50 - 1);
-    return Math.round(quantity);
-};
-
+// Create instances of the Drug class
 var drugList = [
-    cocaine = new Drug("Cocaine", 15000, 28000, 40),
-    weed = new Drug("Weed", 200, 850, 100),
-    heroin = new Drug("Heroin", 5000, 15000, 70),
-    acid = new Drug("Acid", 1000, 3000, 25),
-    speed = new Drug("Speed", 80, 250, 65),
-    opium = new Drug("Opium", 500, 10000, 20)
+    cocaine = new Drug("Cocaine", 15000, 28000),
+    weed = new Drug("Weed", 200, 850),
+    heroin = new Drug("Heroin", 5000, 15000),
+    acid = new Drug("Acid", 1000, 3000),
+    speed = new Drug("Speed", 80, 250),
+    opium = new Drug("Opium", 500, 10000)
 ];
 
 var drugMenu = drugList.map(function(drug) {
