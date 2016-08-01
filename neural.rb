28406c0c84cd5f19a5ef71bb2de43ca1f7890ff8
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-01 15:44:02 -0700

    Add inventory and weapon holster to Player class

--- neural.rb
+++ neural.rb
@@ -3,6 +3,24 @@ class Player
 		@health = health
 		@cash = cash
 		@debt = debt
+
+		# Neural implants:
+		@inventory = {
+			"spike": 0,		# Gives one free turn
+			"flex": 0,		# Increase carry capacity for 5 turns
+			"rage": 0,		# Increase damage by 10%
+			"sight": 0,		# Preview next turn's price changes
+			"sleep": 0,		# Lose 5 days
+			"swap": 0,		# Decrease prices by 10%
+			"fount": 0		# Regenerate health
+		}
+
+		# Holster - adding a weapon removes previous weapon
+		@weapon = ""
+			# Weapons:
+			# KLAW: simple club, low damage
+			# 10mm: 3-shot pistol with reflex sight, medium damage
+			# UTS-15: Large-magazine shotgun, high damage
 	end
 
 	def get_name
@@ -27,14 +45,33 @@ class Player
 		@debt = 0
 	end
 
+	def change_inventory(item, amount)
+		@inventory[item] += amount
+	end
+
+	def display_inventory
+		@inventory.each { |item, amount|
+			print "#{item.capitalize}: #{amount}\n" if amount > 0
+		}
+	end
+
+	def add_weapon(weapon)
+		@weapon = weapon
+	end
+
 	def display
 		puts "Name: #{@name}"
-		puts "Health: #{@health}"
-		puts "Cash: #{@cash}"
-		puts "Debt: #{@debt.round}"
+		puts "Health: #{@health}%"
+		puts "Cash: $#{@cash}"
+		puts "Debt: $#{@debt.round}"
+		puts "Weapon: #{@weapon.upcase}" unless @weapon == ''
 		puts
 	end
 end
 
 new_game = Player.new
 new_game.get_name
+new_game.display
+gets.chomp
+new_game.add_weapon("UTS-15")
+new_game.display
