3817e3ef118ef1c30f4f19e1bb661bb05d2ce51d
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-01 16:59:11 -0700

    Implement method to move stations outside Station class

--- station.rb
+++ station.rb
@@ -1,38 +1,48 @@
 # Stations will be identical for MVP
 # Make stations differ on police/mugger presence, implant availability
 
-class Station
-	def initialize(station)
-		@station = station
-		@locations = [
+
+# Container for all stations
+@locations = [
 			'Downtown',
 			'Chinatown',
 			'Hollywood',
 			'Venice Beach',
 			'Koreatown',
 			'South Central'
-		]
+]
+
+class Station
+	def initialize(station)
+		@station = station
 	end
 
 	def display_station
 		print "Welcome to #{@station}.\n"
 	end
 
-	# Prompt player to select new location
-	def move_to
-		(0...@locations.length).each { |n|
-			print "#{n + 1}: #{@locations[n]}\n"
-		}
+end
 
-		puts "Select a new location:"
-		new_location = gets.chomp.downcase
-		# puts new_location
+# Prompt player to select new location
+def move_to
+	(0...@locations.length).each { |n|
+		print "#{n + 1}: #{@locations[n]}\n"
+	}
 
+	puts "Select a new location by selecting its number:"
+	new_location = gets.chomp.to_i
 
-
+	# Ensure selection is valid
+	if new_location == 0 || new_location > 6
+		puts "\n" * 10
+		puts "That is not a valid selection, try again."
+		move_to
+	else
+		@locations[new_location - 1]
 	end
 end
 
 location = Station.new('Downtown')		# Always start in Downtown
 location.display_station
-location.move_to
+location = Station.new(move_to)
+location.display_station
