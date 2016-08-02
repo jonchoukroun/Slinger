291bb2d47831adca5573360b7b444bdf2d916d41
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-02 11:07:13 -0700

    Implement methods to display player stats and inventory

--- Ruby/game.rb
+++ Ruby/game.rb
@@ -3,12 +3,16 @@ require_relative 'player'
 require_relative 'station'
 
 class Game
+	# Constant line break to puts dashes
+	BREAK = "\n" + "-" * 50 + "\n"
+
 	def initialize
 		@turns = 5		# Increase to 30 for full game
 
 		# Create player
 		@new_player = Player.new
 		@new_player.get_name
+		@new_player.change_inventory(:spike, 20)
 
 		# Start downtown
 		@current_station = Station.new("Downtown")
@@ -23,11 +27,12 @@ class Game
 			'South Central'
 		]
 
-		# Possible player actions
-		@actions = [
+		# Possible player choices at the station menu
+		@station_choices = [
 			'Visit black market',
 			'Pay off loan shark',
-			'Change location'
+			'Change location',
+			'View inventory'
 		]
 
 	end
@@ -47,8 +52,21 @@ class Game
 			@new_player.incur_debt
 			@current_station.display_station
 			puts "You have #{@turns} turns left."
+			# Display player stats
 			game_menu
 		end
+	end
+
+	def display_stats
+		puts BREAK
+		puts """
+		#{@new_player.name}' Statistics:
+		--------------------------------
+			Health: #{@new_player.health}
+			Cash: $#{@new_player.cash}
+			Debt: $#{@new_player.debt}
+		"""
+		puts BREAK
 	end
 
 	def make_selection(options)
@@ -74,20 +92,26 @@ class Game
 	end
 
 	def black_market
-		puts "\n"
+		puts BREAK
 		@current_station.implants_menu
+		puts BREAK
+		game_menu
 	end
 
 	def loan_shark
 		if @new_player.cash >= @new_player.debt
 			@new_player.debt = 0
 			@new_player.cash -= @new_player.debt
+			game_menu
 		else
+			puts BREAK
 			puts "You do not have enough cash to pay your debts."
+			game_menu
 		end
 	end
 
 	def change_location
+		puts BREAK
 		puts "Where will you go?"
 		new_location = make_selection(@locations)
 		@current_station = Station.new(@locations[new_location - 1])
@@ -95,12 +119,23 @@ class Game
 		new_turn
 	end
 
+	def display_inventory
+		puts BREAK
+		puts "Implants in possession:"
+		puts BREAK
+		@new_player.display_inventory
+		puts BREAK
+		game_menu
+	end
+
 	def game_menu
+		puts BREAK
 		puts "What will you do?"
-		case make_selection(@actions)
+		case make_selection(@station_choices)
 		when 1 then black_market
 		when 2 then loan_shark
 		when 3 then change_location
+		when 4 then display_inventory
 		end			
 	end
 
@@ -108,4 +143,5 @@ class Game
 end
 
 play = Game.new
-play.new_turn
+# play.new_turn
+play.display_stats
