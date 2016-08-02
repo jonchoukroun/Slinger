050b8f39f26174c372436e0500a406cd855edbd7
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-02 12:38:41 -0700

    Standardize methods for return to menu options and screen clearing

--- Ruby/game.rb
+++ Ruby/game.rb
@@ -4,13 +4,17 @@ require_relative 'station'
 
 class Game
 	# Constant line break to puts dashes
-	BREAK = "\n" + "-" * 50 + "\n"
+	BREAK = "\n" + "-" * 80 + "\n"
+
+	# Prompt user to press ENTER
+	CONTINUE = "Press ENTER to continue..."
 
 	def initialize
 		@turns = 5		# Increase to 30 for full game
 
 		# Create player
 		@new_player = Player.new
+		system 'clear'
 		@new_player.get_name
 		@new_player.change_inventory(:spike, 20)
 
@@ -24,21 +28,37 @@ class Game
 			'Hollywood',
 			'Venice Beach',
 			'Koreatown',
-			'South Central'
+			'South Central',
+			'Return to menu'
 		]
 
 		# Possible player choices at the station menu
 		@station_choices = [
 			'Visit black market',
 			'Pay off loan shark',
-			'Change location',
-			'View inventory'
+			'View inventory',
+			'Change location (end turn)'
 		]
 
 	end
 
 	def finished?
-		@turns < 0 || @new_player.health == 0
+		@turns < 0
+	end
+
+	def end_game
+		system 'clear'
+		puts BREAK
+		puts """
+		Game Over!
+
+		After 30 days, you made:
+			$#{(@new_player.cash - @new_player.debt).round}
+		"""
+
+		# Place holder for score saving option on UI
+		puts "Save score (y/n)?"
+		gets.chomp
 	end
 
 	def take_turn
@@ -48,48 +68,63 @@ class Game
 	def start_game
 		puts """
 		#{BREAK}
-		Welcome to $$NEW_TITLE$$
-		------------------------
+					SLINGER 2080
+		------------------------------------
 
-		Los Angeles, 2070...
+		Los Angeles, 2065...
 
 		GeneTech Inc. invents the neural implant: nano-bots that supercharge
 		human ability and improve speed, strength, the senses...
 		... at an astronomical cost. 
 
-		The gap between between rich and poor expands,as those who can afford
-		implants become essentially super-human. Poverty and crime surpass
-		levels common in the 3rd world.
+		The gap between between rich and poor expands, as those who can afford
+		implants become more than human. Poverty and crime surpass levels common
+		in the 3rd world.
 		#{BREAK}
 
-		15 years later...
+		#{CONTINUE}
+		"""
+		gets.chomp
+		system 'clear'
 
+		puts """
+
+		#{BREAK}
 		A GeneTech engineer leaks the key technology behind neural implantation,
 		creating a booming black market in powerful - but dangerous - implants.
-		These \"NIMs\", engineered in basement labs offer a greater variety
-		of abilities, but the risk of malfunction is high... and sometimes
-		fatal. The high use of NIMs has turned number of the city's desperate
-		into feral killers called NIMscum.
+		These \"NIMs\", engineered in basement labs, offer a greater variety
+		of abilities, but the risk of malfunction is high... The high use of
+		NIMs has turned a number of the city's desperate into feral killers
+		called NIMscum.
 
 		As a slinger, you buy and sell NIMs on LA's unforgiving streets. You
 		have 30 days to earn as much as possible, but look out for other
-		dealers, GeneTech security, and the NIMscum. Maybe invest in some
-		protection...
+		dealers, GeneTech security, and the NIMscum. And pay back your loan to
+		GeneTech Bank before they send collectors after you.
+		Maybe invest in protection...
 
 		If things get too tough, taking NIMs can always give you an edge - but
 		are they worth the risk?
 		#{BREAK}
 		Good luck, #{@new_player.name}
 		"""
+		puts CONTINUE
+		gets.chomp
+		system 'clear'
+
+		# First turn, doesn't incur more debt, no possibility of dying
+		# No random events
+		@current_station.display_station
+		game_menu
 	end
+
 	def new_turn
 		if finished?
-			puts "Game over!"
+			end_game
 		else
+			system 'clear'
 			@new_player.incur_debt
 			@current_station.display_station
-			puts "You have #{@turns} turns left."
-			# Display player stats
 			game_menu
 		end
 	end
@@ -97,11 +132,13 @@ class Game
 	def display_stats
 		puts BREAK
 		puts """
-		#{@new_player.name}' Statistics:
+		#{@new_player.name}'s Statistics:
 		--------------------------------
 			Health: #{@new_player.health}%
 			Cash: $#{@new_player.cash}
-			Debt: $#{@new_player.debt}
+			Debt: $#{@new_player.debt.round}
+		--------------------------------
+		Turns left: #{@turns}
 		"""
 		puts BREAK
 	end
@@ -129,20 +166,39 @@ class Game
 	end
 
 	def black_market
+		system 'clear'
 		puts BREAK
 		@current_station.implants_menu
-		puts BREAK
+
+		# Implement transactions methods
+
+		# Return to menu
+		puts CONTINUE
+		gets.chomp
+		system 'clear'
 		game_menu
 	end
 
 	def loan_shark
 		if @new_player.cash >= @new_player.debt
-			@new_player.debt = 0
 			@new_player.cash -= @new_player.debt
+			@new_player.debt = 0
+			puts """
+			Your debt is paid and you are safe from GeneTech Bank collectors.
+			"""
+			# Return to menu
+			puts CONTINUE
+			gets.chomp
+			system 'clear'
 			game_menu
 		else
 			puts BREAK
 			puts "You do not have enough cash to pay your debts."
+			
+			# Return to menu
+			puts CONTINUE
+			gets.chomp
+			system 'clear'
 			game_menu
 		end
 	end
@@ -151,28 +207,38 @@ class Game
 		puts BREAK
 		puts "Where will you go?"
 		new_location = make_selection(@locations)
-		@current_station = Station.new(@locations[new_location - 1])
-		take_turn
-		new_turn
+		if new_location == 7
+			system 'clear'
+			game_menu
+		else
+			@current_station = Station.new(@locations[new_location - 1])
+			take_turn
+			new_turn
+		end
 	end
 
 	def display_inventory
 		puts BREAK
 		puts "Implants in possession:"
-		puts BREAK
 		@new_player.display_inventory
 		puts BREAK
+		
+		# Return to menu
+		puts CONTINUE
+		gets.chomp
+		system 'clear'
 		game_menu
 	end
 
 	def game_menu
+		display_stats
 		puts BREAK
-		puts "What will you do?"
+		puts "What do you do?"
 		case make_selection(@station_choices)
 		when 1 then black_market
 		when 2 then loan_shark
-		when 3 then change_location
-		when 4 then display_inventory
+		when 3 then display_inventory
+		when 4 then change_location
 		end			
 	end
 
