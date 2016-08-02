1a01b381f8709d01324d7a74e12ad9ee887389f1
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-01 19:07:34 -0700

    Move locations array to Game class

--- station.rb
+++ station.rb
@@ -2,16 +2,6 @@
 # Make stations differ on police/mugger presence, implant availability
 
 
-# Container for all stations
-@locations = [
-			'Downtown',
-			'Chinatown',
-			'Hollywood',
-			'Venice Beach',
-			'Koreatown',
-			'South Central'
-]
-
 class Station
 	# Constant Q = Quantity
 	Q = 0
@@ -52,31 +42,9 @@ class Station
 	end
 end
 
-# Prompt player to select new location
-def move_to
-	puts "\n" * 2
-	puts "Moving..."
-	puts "-" * 50
-	(0...@locations.length).each { |n|
-		print "#{n + 1}: #{@locations[n]}\n"
-	}
-
-	puts "Select a new location by selecting its number:"
-	new_location = gets.chomp.to_i
-
-	# Ensure selection is valid
-	if new_location == 0 || new_location > 6
-		puts "\n" * 10
-		puts "That is not a valid selection, try again."
-		move_to
-	else
-		@locations[new_location - 1]
-	end
-end
-
-location = Station.new('Downtown')		# Always start in Downtown
-location.display_station
-location.implants_menu
-location = Station.new(move_to)
-location.display_station
-location.implants_menu
+# location = Station.new('Downtown')		# Always start in Downtown
+# location.display_station
+# location.implants_menu
+# location = Station.new(move_to)
+# location.display_station
+# location.implants_menu
