f0bf49893cfc0fda090190698d73b8a518308ec8
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-01 19:05:37 -0700

    Implement menu, select location, validate selection, and game over methods

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ require_relative 'station'
 
 class Game
 	def initialize
-		@turns = 5
+		@turns = 5		# Increase to 30 for full game
 
 		# Create player
 		@new_player = Player.new
@@ -32,8 +32,12 @@ class Game
 
 	end
 
+	def finished?
+		@turns < 0
+	end
+
 	def take_turn
-		@turns -= 1
+		@turns -=1 
 	end
 
 	def make_selection(options)
@@ -44,24 +48,36 @@ class Game
 		choice = gets.chomp.to_i
 
 		# Validate player selection
-		if selection.between?(1..range.length)
+		if choice.between?(1, options.length)
 			choice
 		else
 			puts "That is not a valid selection. Make sure to enter an integer."
 			make_selection(options)
+		end
 	end
 
 	def select_location
 		puts "Where will you go?"
 		new_location = make_selection(@locations)
+		@current_station = Station.new(@locations[new_location - 1])
+		take_turn
+		menu
 	end
 
 	def menu
-		@current_station.display_station
-		puts "You have #{@turns} turns left."
-		puts
-		puts "What will you do?"
-		action = make_selection(@actions)
+		if finished?
+			puts "Game over!"
+		else
+			@current_station.display_station
+			puts "You have #{@turns} turns left."
+			puts
+			puts "What will you do?"
+			case make_selection(@actions)
+			when 1 then puts "Black Market"
+			when 2 then puts "Loan shark"
+			when 3 then select_location
+			end			
+		end
 	end
 
 
