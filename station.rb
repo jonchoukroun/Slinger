7dacd861d08c187d72c9b4f09c7ab4b3ff0b8aaf
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-05 11:22:59 -0700

    Fix demand-based price methods to only calculate on station initialization, preventing arbitrage creation

--- station.rb
+++ station.rb
@@ -8,33 +8,33 @@ class Station
 	attr_accessor :station
 	attr_reader :implants
 
-	# Constants to access implant data array
+	# Constants to access implant price data array
 	# Q = Quantity
 	Q = 0
-	# M = Slope
-	M = 1
-	# B = Y-Intercept
-	B = 2
+	# P = Price
+	P = 1
 
 	def get_quantity(max)
 		rand(0..max)
 	end
 
 	def get_price(q, m, b)
-		q * m + b
+		q = get_quantity(q)
+		[q, (q * m + b)]
+
 	end
 
 	def initialize(station)
 		@station = station
 		@implants = {
-			# implant: [max_quantity, slope, y-intercept]
-			"spike": [get_quantity(45), -17, 1300],		# Gives 1 free turn
-			"flex": [get_quantity(65), -9, 900],			# Increase carry capacity 20%
-			"rage": [get_quantity(50), -21, 1500],		# Increase damage 20%
-			"sight": [get_quantity(10), -1000, 25000],	# Preview price changes
-			"sleep": [get_quantity(100), -0.55, 65],		# Lose a turn
-			"swap": [get_quantity(30), -83, 3500],		# Reduce prices 10%
-			"fount": [get_quantity(20), -450, 14000]		# Restore health
+			# implant: [get_price(max_quantity, slope, y-intercept)]
+			"spike": get_price(45, -17, 1300),		# Gives 1 free turn
+			"flex": get_price(65, -9, 900),			# Increase carry capacity 20%
+			"rage": get_price(50, -21, 1500),			# Increase damage 20%
+			"sight": get_price(10, -1000, 25000),		# Preview price changes
+			"sleep": get_price(100, -0.55, 65),		# Lose a turn
+			"swap": get_price(30, -83, 3500),			# Reduce prices 10%
+			"fount": get_price(20, -450, 14000)		# Restore health
 		}
 
 	end
@@ -55,8 +55,7 @@ class Station
 		puts "Implants available:"
 		puts "-" * 50
 		current_implants.each { |implant, data|
-			price = get_price(data[Q], data[M], data[B])
-			puts "#{implant.capitalize}: #{data[Q]} @ $#{price.round}\n"
+			puts "#{implant.capitalize}: #{data[Q]} available at $#{data[P].round}\n"
 		}
 	end
 end
