999d631f9192d2d2e25d037f450da0846be35b98
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-05 11:48:49 -0700

    Mock up skeleton of Events class

---
+++ events.rb
@@ -0,0 +1,29 @@
+# Contains all methods defining and executing random events
+# MVP will not include fighting mechanism
+
+# Call on Player class methods
+require_relative 'player'
+class Events
+	def initialize
+		@debt = @new_player.debt
+		@inventory = @new_player.inventory
+	end
+
+	def collector
+	end
+
+	def glitcher
+	end
+
+	def package
+	end
+	
+	def positive?
+	end
+
+	def occurence?
+	end
+
+end
+
+test = Events.new
