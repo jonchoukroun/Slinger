d1ce8dbbbfe70ca4bc5a3c8fd36bc2edf16e9663
Jon Choukroun <jonchoukroun@gmail.com>
2016-08-07 22:20:33 -0700

    Add TODO list to rewrite class

--- events.rb
+++ events.rb
@@ -5,12 +5,11 @@
 require_relative 'player'
 
 class Events
-	def initialize(health, debt, inventory)
+	def initialize
+		@health = health
 		@inventory = inventory
 		@debt = debt
 	end
-
-	# Alll events return array [health, cash, [implant, amount]]
 
 	# Probability of collector rises each time debt increase by 1000
 	def collector
@@ -32,18 +31,19 @@ class Events
 		puts "Press ENTER to continue..."
 		gets.chomp
 
-		return [-30, 0, [null, 0]]
+		@health += -30
 
 	end
 
+	# Glitcher takes all implants in inventory
 	def glitcher
-		return false unless rand(0..100) < 10
+		return false unless rand(0..100) < 100		# Change to 5
 
 		@inventory.each { |implant, amount|
-			@new_player.change_inventory(implant, -amount)
+			print "i = #{implant}, a = #{amount}\n"
 		}
 
-		system 'clear'
+		# system 'clear'
 		puts """
 		A Glitcher corners you on the subway platform.
 
@@ -51,11 +51,12 @@ class Events
 		Without a word she slams you into a wall and tears into your coat.
 		She takes all your implants, but at least you're still alive.
 		"""
-
+		puts @inventory
 		puts "Press ENTER to continue..."
 		gets.chomp
 	end
 
+	# Pick implant to find package of
 	def random_implant
 		@inventory.keys[rand(0...@inventory.length)]
 	end
@@ -80,12 +81,16 @@ class Events
 	end
 
 	def occurence?
-		collector if @debt > 0
+		@health += collector if @debt > 0
 		glitcher if @inventory.each_value.reduce(:+) > 0
 		package
 	end
 end
 
 
-test = Events.new
-test.collector
+test = Events.new(100, 1000000, {'spike': 10, 'flex': 5})
+puts test.glitcher
+
+# TODO
+# Rewrite class
+# Should this be part of game file?
